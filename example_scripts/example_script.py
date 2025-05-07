cee1f42fc681536a3ad79aa0075203d9c37456e5
Marc Hadfield <marc@vital.ai>
2025-05-07 15:30:42 -0400

    sync

---
+++ example_scripts/example_script.py
@@ -0,0 +1,12 @@
+from vital_ai_vitalsigns.vitalsigns import VitalSigns
+
+def main():
+    print('Example Script')
+
+    vs = VitalSigns()
+
+    print("VitalSigns Initialized")
+
+if __name__ == "__main__":
+    main()
+
--- kgraph_examples/kgraph_clear_db_example.py
+++
@@ -1,9 +0,0 @@
-
-def main():
-    print('Hello World')
-    # given graph name, delete from graph db
-    # delete classes (collections) from vector db in graph namespace
-
-
-if __name__ == "__main__":
-    main()
